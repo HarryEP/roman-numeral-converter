e86a8654c8d4a3c2dde4ed975b7a5544ed2b1585
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-01-16 11:10:24 +0000

    set up more tests

--- test_main.py
+++ test_main.py
@@ -1,6 +1,24 @@
-from main import word_to_numeral
+import pytest
+from main import word_to_numeral, check_valid_roman_numeral
 
 
 def test_nine_as_word_goes_numeral():
     word = "IX"
     assert word_to_numeral(word) == 9
+
+
+def test_word_too_high_is_invalid():
+    word = "MMMMMXVI"
+    with pytest.raises(Exception):
+        word_to_numeral(word)
+
+
+def test_negative_number_is_valid():
+    word = "-MCLXI"
+    assert word_to_numeral(word) == -1161
+
+
+def test_four_repetitive_letters_is_invalid():
+    word = "MCDCCXXXXVI"
+    with pytest.raises(Exception):
+        check_valid_roman_numeral(word)
