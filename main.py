fe795a5e14673cdf2769dd8f127229b163f92061
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-01-21 18:42:39 +0000

    trying to fix 9 and 4 issues

--- main.py
+++ main.py
@@ -30,23 +30,47 @@ def numeral_to_word(number: int) -> str:
             word = add_new_letter(word, 'M')
             temp -= 1000
         elif temp // 500 != 0:
-            word = add_new_letter(word, 'D')
-            temp -= 500
+            if (temp + 100) // 1000 != 0:
+                word += 'CM'
+                temp -= 900
+            else:
+                word = add_new_letter(word, 'D')
+                temp -= 500
         elif temp // 100 != 0:
-            word = add_new_letter(word, 'C')
-            temp -= 100
+            if (temp + 100) // 500 != 0:
+                word += 'CD'
+                temp -= 400
+            else:
+                word = add_new_letter(word, 'C')
+                temp -= 100
         elif temp // 50 != 0:
-            word = add_new_letter(word, 'L')
-            temp -= 50
+            if (temp + 10) // 100 != 0:
+                word += 'XC'
+                temp -= 90
+            else:
+                word = add_new_letter(word, 'L')
+                temp -= 50
         elif temp // 10 != 0:
-            word = add_new_letter(word, 'X')
-            temp -= 10
+            if (temp + 10) // 50 != 0:
+                word += 'XL'
+                temp -= 40
+            else:
+                word = add_new_letter(word, 'X')
+                temp -= 10
         elif temp // 5 != 0:
-            word = add_new_letter(word, 'V')
-            temp -= 5
+            if (temp + 100) // 500 != 0:
+                word += 'IX'
+                temp -= 9
+            else:
+                word = add_new_letter(word, 'V')
+                temp -= 5
         elif temp // 1 != 0:
-            word = add_new_letter(word, 'I')
-            temp -= 1
+            if (temp + 1) // 5 != 0:
+                word += 'IV'
+                temp -= 4
+            else:
+                word = add_new_letter(word, 'I')
+                temp -= 1
         print(word, temp)
     return word
 
